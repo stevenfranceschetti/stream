76c7c289fed3e7c3bf0c58638c28bed77be08402
Steven Franceschetti <steven.franceschetti@gmail.com>
2026-06-02 02:08:26 -0400

    add opening scene with twins

---
+++ Story.py
@@ -0,0 +1,14 @@
+# Story
+print("Welcome to Stream!")
+print(""" 
+The air feels thick, breath from the city itself. Sweat is already forming on your back.
+      
+A child is staring at you with head slightly tilted to the left.
+      
+“You just arrived from the nothing? Are you a ghost?”
+      
+“Don’t be silly, ” said their twin walking up from behind, “Ghosts are made of stone. They look squishy. And they are sweating.” 
+
+“Maybe you are forming.” said the first.
+
+“Or dying?” said the second.""")
